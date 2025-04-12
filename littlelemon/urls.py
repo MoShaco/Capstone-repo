cbc1b6d8de95662f96bd7e1041e34dab021cc74f
Mohammed Shaco <mohammedshaco@gmail.com>
2025-04-12 15:45:46 +0200

    update (urls.py) in project leavel: remove defalut route that belongs to returant.urls

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,17 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from restaurant import views
 
-router = DefaultRouter()
-router.register(f'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/menu/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
-    # Authentication
+    path('restaurant/', include('restaurant.urls')),
+
+    # Djoser Authentication endpoints
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
