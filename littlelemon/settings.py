3fd93f80dec1e1dc41b3c7798f471ed33350b879
Mohammed Shaco <mohammedshaco@gmail.com>
2025-04-12 15:44:50 +0200

    update (settings.py): add the user that has privillages to create and destroy databases

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,8 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
-        'USER': 'django',
-        'PASSWORD': 'django',
+        # 'USER': 'django',
+        # 'PASSWORD': 'django',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
